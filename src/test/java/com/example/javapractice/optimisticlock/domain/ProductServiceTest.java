94df65362da66f2429f1658bde5b29bb7d55983a
philE22 <moonwhlking@gmail.com>
2025-08-06 23:51:44 +0900

    낙관적락 에러 확인

--- src/test/java/com/example/javapractice/optimisticlock/domain/ProductServiceTest.java
+++ src/test/java/com/example/javapractice/optimisticlock/domain/ProductServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.javapractice.optimisticlock.domain;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,9 +12,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @SpringBootTest
 class ProductServiceTest {
 
@@ -48,19 +52,29 @@ class ProductServiceTest {
     }
 
     @Test
-    void 낙관적락() throws ExecutionException, InterruptedException {
+    void 낙관적락_에러_테스트() throws ExecutionException, InterruptedException {
         Product product = Product.builder()
                 .name("아이폰")
                 .quantity(100)
                 .build();
         Long savedId = productService.save(product);
 
-        try (ExecutorService es = Executors.newFixedThreadPool(10)) {
+        int threadCount = 10;
+        AtomicInteger successCount = new AtomicInteger();
+        AtomicInteger failureCount = new AtomicInteger();
+
+        try (ExecutorService es = Executors.newFixedThreadPool(threadCount)) {
             List<Future<Void>> futures = new ArrayList<>();
 
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < threadCount; i++) {
                 Future<Void> future = es.submit(() -> {
-                    productService.decreaseStock(savedId, 1);
+                    try {
+                        productService.decreaseStock(savedId, 1);
+                        successCount.incrementAndGet();
+                    } catch (ObjectOptimisticLockingFailureException e) {
+                        log.error("낙관적락 실패 처리 필요!");
+                        failureCount.incrementAndGet();
+                    }
                     return null;
                 });
                 futures.add(future);
@@ -70,25 +84,13 @@ class ProductServiceTest {
             }
         }
 
+        log.info("성공한 시도: {}", successCount.get());
+        log.info("실패한 시도: {}", failureCount.get());
+
         Product result = productService.findBy(savedId);
-        assertThat(result.getQuantity()).isEqualTo(90);
+        assertThat(result.getQuantity()).isEqualTo(100 - successCount.get());
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //TODO 낙관적락 / 비관적락 / synchronized 성능 비교 테스트도 진행
 
 }
